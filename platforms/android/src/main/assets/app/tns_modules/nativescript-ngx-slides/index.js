225bee90b056a70a1fd2cc0e4ebeecee9cb35c2a
Matt <matt@Matts-MacBook-Pro.local>
2017-07-28 08:01:01 -0400

    remove generated files from repo

--- platforms/android/src/main/assets/app/tns_modules/nativescript-ngx-slides/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var slides_module_1 = require("./slides/app/slides.module");
-exports.SlidesModule = slides_module_1.SlidesModule;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDREQUEwRDtBQUFqRCx1Q0FBQSxZQUFZLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgeyBTbGlkZXNNb2R1bGUgfSBmcm9tICcuL3NsaWRlcy9hcHAvc2xpZGVzLm1vZHVsZSc7Il19
--- platforms/ios/Pods/Target Support Files/MNFloatingActionButton/MNFloatingActionButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MNFloatingActionButton : NSObject
-@end
-@implementation PodsDummy_MNFloatingActionButton
-@end
